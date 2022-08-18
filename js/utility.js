59eeb58e0261403536c5038fe2d4e3dd6a3e7ae4
Jharna203 <jharnakhatun203@gmail.com>
2022-08-19 00:06:57 +0600

    responsive penguin fashion by tailwind

---
+++ js/utility.js
@@ -0,0 +1,4 @@
+document.getElementById('buy-now').addEventListener('click',function(){
+    const beThePenguin = document.getElementById('be-penguin');
+    beThePenguin.innerText = 'I AM PENGUIN';
+    });
